d6ebd94cc526fc20d3972bd646597801f5063601
schmijc4156 <jacobschmitt02@gmail.com>
2017-08-04 13:02:07 -0600

    pythagorean tripples
    checks if a triangle is a right triangle

--- pythagtrips.py
+++ pythagtrips.py
@@ -1,9 +1,19 @@
-will_smart = input("do you think will is smart? 1 for yes. 2 for no   ")
-def will_determin(inteligence):
-    if will_smart == str(2):
-        print("you are wrong")
-    elif will_smart == str(1):
-        print("you are correct")
-    elif will_smart <= str(3):
-        print("try again")
-print(will_determin(will_smart))
+tri_sides = 3
+tri_side_list = []
+
+while True:
+    for x in range(int(tri_sides)):
+        tri_side_list.append(input("enter your sides: "))
+        tri_side_list.sort()
+        print(tri_side_list)
+    def tri_math(tri_side_list):
+        if int(tri_side_list[0]) ** 2 + int(tri_side_list[1]) ** 2 == int(tri_side_list[2]):
+            print("this is a right triangle")
+        else:
+            print("this is not a right triangle")
+    print(tri_math(tri_side_list))
+    do_over = 1
+    print("play again?")
+    do_over = input("1 yes, 2 no: ")
+    if do_over == str(2):
+        break
