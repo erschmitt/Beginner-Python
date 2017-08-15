0cc50666b452d25b35aed03a325e2cae030a5e1a
schmijc4156 <jacobschmitt02@gmail.com>
2017-08-15 15:16:44 -0600

    final for mad libs and pythagorean tripples

--- MadLibs.py
+++ MadLibs.py
@@ -23,31 +23,25 @@ verbing = input("now a verb ending in 'ing' ")
 plrn3 = input("another plural noun ")
 verb4 = input("and finally the last one, a verb ")
 a = input("jk heres another noun ")
+word = []
+word.append(a)
+def figaan(x):
+    if str(word[0][0]) in "aeiouAEIOU":
+        aAN = "an"
+    else:
+        aAN = "a"
+    return(aAN)
+aan = figaan(word)
 print("adding your words...")
 print()
 print()
-word = ['a', 'an']
-wordlist = []
-wordlist.append(a)
-print(wordlist[0][0])
-vowel1 = "a"
-vowel2 = "e"
-vowel3 = "i"
-vowel4 = "o"
-vowel5 = "u"
-print()
-def lookatvowels(x):
-    if a[0][0] == str(vowel1) or str(vowel2) or str(vowel3) or str(vowel4) or str(vowel5):
-        print(word[1])
-    else:
-        print(word[0])
-print(lookatvowels(word))
-time.sleep(5)
+
+time.sleep(1)
 print("Congratulations! You finally managed to get a job interview at %s. Follow these a steps and you'll be %s for sure!" % (company_name.title(), adj1))
 print("1  Make sure you dress appropriately. You should probably wear something like a %s" % (clothing))
 print("Make sure its not too %s or %s. Prospective employers do not like to see too much %s." % (adj2, adj3, adj4))
 print("2  Be sure to %s and %s your %s before the interview. Make sure your breath is %s" % (Verb1, noun, adj5, adj8))
-print("3  Smile, be %s and remember a firm hand shake is always %s %s" % (adj6, lookatvowels(word), a))
+print("3  Smile, be %s and remember a firm hand shake is always %s %s" % (adj6, aan, a))
 print("4  Make sure to mention such topics as %s, %s and %s." % (noun2, noun3, plrn))
 print("Be sure to avoid talking about %s They might get the wrong impression and think you are too %s." % (plrn2, adj7))
 print("5  You may want to compliment your potential boss on his her %s and mention how much you enjoyed %s with them." % (noun4, verbing))
--- pythagtrips.py
+++ pythagtrips.py
@@ -5,9 +5,8 @@ while True:
     for x in range(int(tri_sides)):
         tri_side_list.append(input("enter your sides: "))
         tri_side_list.sort()
-        print(tri_side_list)
     def tri_math(tri_side_list):
-        if int(tri_side_list[0]) ** 2 + int(tri_side_list[1]) ** 2 == int(tri_side_list[2]):
+        if int(tri_side_list[0]) ** 2 + int(tri_side_list[1]) ** 2 == int(tri_side_list[2]) ** 2:
             print("this is a right triangle")
         else:
             print("this is not a right triangle")
@@ -15,5 +14,7 @@ while True:
     do_over = 1
     print("play again?")
     do_over = input("1 yes, 2 no: ")
+    if do_over == str(1):
+        tri_side_list.pop
     if do_over == str(2):
         break
